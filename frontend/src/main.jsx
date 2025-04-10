625f337a9c05ecb7fffdcf3725aaf773e1fc3a17
jmei403 <jason.mei403@gmail.com>
2025-04-10 13:32:07 -0400

    move styles folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+import './styles/bootstrap.custom.css';
+import './styles/index.css';
 import App from './App.jsx';
 import HomePage from './pages/HomePage.jsx';
 
