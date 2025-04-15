4151f30f7dafd07d898b1dfabe42e78fe75ef052
jmei403 <jason.mei403@gmail.com>
2025-04-15 16:47:04 -0400

    redux toolkit setup and apiSlice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
 import './styles/bootstrap.custom.css';
 import './styles/index.css';
 import App from './App.jsx';
@@ -19,6 +20,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
