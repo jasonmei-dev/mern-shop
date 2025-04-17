6dba2d7a1eb455a1a5f3047207eafcb41339a5d4
jmei403 <jason.mei403@gmail.com>
2025-04-17 19:03:29 -0400

    show cart item count in navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
+  console.log(cartItems);
   return (
     <header>
       <Navbar variant="dark" bg="dark" expand="md" collapseOnSelect>
@@ -16,6 +20,17 @@ const Header = () => {
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge
+                    pill
+                    bg="success"
+                    style={{
+                      marginLeft: '5px',
+                    }}
+                  >
+                    {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                  </Badge>
+                )}
               </Nav.Link>
               <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
