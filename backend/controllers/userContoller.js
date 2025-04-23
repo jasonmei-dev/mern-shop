74a6a1aeabea263e399331b99d06567053b37ab8
jmei403 <jason.mei403@gmail.com>
2025-04-22 22:27:48 -0400

    register new user functionality

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -29,6 +29,12 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('All fields required');
+  }
+
   const userExists = await User.findOne({ email });
 
   if (userExists) {
