e69da5dcfd13351404db34b996758d906075fba1
jmei403 <jason.mei403@gmail.com>
2025-04-23 16:42:50 -0400

    style adjustment for loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,25 +2,25 @@ import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
   return (
-    <div
+    // <div
+    //   style={{
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     height: '80vh',
+    //   }}
+    // >
+    <Spinner
+      animation="border"
+      role="status"
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '80vh',
+        width: '100px',
+        height: '100px',
+        margin: 'auto',
+        display: 'block',
       }}
-    >
-      <Spinner
-        animation="border"
-        role="status"
-        style={{
-          width: '100px',
-          height: '100px',
-          margin: 'auto',
-          display: 'block',
-        }}
-      ></Spinner>
-    </div>
+    ></Spinner>
+    // </div>
   );
 };
 
