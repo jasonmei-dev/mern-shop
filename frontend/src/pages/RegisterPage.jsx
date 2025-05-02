15d9589d65b54568ccfc30e5fd27209f41f7de1a
jmei403 <jason.mei403@gmail.com>
2025-05-02 19:58:50 -0400

    fix form input type for name field

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -51,7 +51,7 @@ const RegisterPage = () => {
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="name" className="my-3">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+          <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
         </Form.Group>
         <Form.Group controlId="email" className="my-3">
           <Form.Label>Email Address</Form.Label>
