508ae0a44ff1796bdee4777423bf2b1a722aacfd
jmei403 <jason.mei403@gmail.com>
2025-05-02 16:51:52 -0400

    admin product list page

--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
 
 const ProductListPage = () => {
-  return <div>ProductListPage</div>;
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const deleteProductHandler = (productId) => {
+    console.log(`Delete product ${productId}`);
+  };
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="btn-sm m-3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table striped hover responsive size="sm" style={{ whiteSpace: 'nowrap' }}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <Button as={Link} to={`/admin/product/${product._id}/edit`} variant="light" className="btn-sm mx-2">
+                      <FaEdit />
+                    </Button>
+                    <Button onClick={() => deleteProductHandler(product._id)} variant="danger" className="btn-sm mx-2">
+                      <FaTrash style={{ color: 'white' }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductListPage;
