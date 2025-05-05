9cfeabe7a016d8732ae82bff572b84d365af40b1
jmei403 <jason.mei403@gmail.com>
2025-05-05 16:35:45 -0400

    multer setup for image file upload

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, 'uploads/');
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const checkFileType = (file, callback) => {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname)).toLowerCase();
+  const mimetype = filetypes.test(file.mimtype);
+  if (extname && mimetype) {
+    return callback(null, true);
+  } else {
+    callback('Images only!');
+  }
+};
+
+const upload = multer({
+  storage,
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send({
+    message: 'Image uploaded',
+    image: `/${req.file.path}`,
+  });
+});
+
+export default router;
