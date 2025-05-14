8b886c0238fcd47c2374b1e8a0a5fe86654b4140
jmei403 <jason.mei403@gmail.com>
2025-05-14 18:53:52 -0400

    bug fix for JWT token expiration not handled in frontend

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants.js';
+import { logout } from './authSlice.js';
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
+const baseQueryWithAuth = async (args, api, extra) => {
+  const result = await baseQuery(args, api, extra);
+
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithAuth,
   tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}),
 });
