4ac4f942facfeedb8a4266cadadbfabb25574073
jmei403 <jason.mei403@gmail.com>
2025-05-15 16:10:11 -0400

    remove redundant loader & typo fix

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error.data?.message || error.error}</Message>
+        <Message variant="danger">{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
